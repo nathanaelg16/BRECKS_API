499a87c84a3bf31395654ef6f5246f8e23f7a3e2
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-03-26 19:34:48 -0400

    persisting access key to db

--- src/main/java/app/brecks/auth/accesskey/AccessKeyManager.java
+++ src/main/java/app/brecks/auth/accesskey/AccessKeyManager.java
@@ -4,21 +4,22 @@ import app.brecks.exception.auth.AccessKeyException;
 import lombok.NonNull;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.Marker;
-import org.apache.logging.log4j.MarkerManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class AccessKeyManager {
     private static final Logger logger = LogManager.getLogger();
-    private static final Marker marker = MarkerManager.getMarker("[Access Key Manager]");
-    private final Map<String, AccessKey> accessKeys;
+    private final AccessKeyRepository accessKeys;
 
-    public AccessKeyManager() {
-        this.accessKeys = new HashMap<>();
+    @Autowired
+    public AccessKeyManager(AccessKeyRepository accessKeyRepository) {
+        this.accessKeys = accessKeyRepository;
     }
 
     // todo define enum of allowed scopes
@@ -28,29 +29,30 @@ public class AccessKeyManager {
     }
 
     public String createAccessKey(@NonNull String email, @NonNull KeyDuration duration, @NonNull Collection<String> scopes) throws AccessKeyException {
-        logger.info("[Access Key Manager] Creating a new access key for user `{}` with duration `{}` and scopes: {}", email, duration.name(), scopes);
-        AccessKey accessKey = new AccessKey(email, duration, new HashSet<>(scopes));
+        logger.info("Creating a new access key for user `{}` with duration `{}` and scopes: {}", email, duration.name(), scopes);
+        AccessKey accessKey = AccessKey.create(email, duration, new HashSet<>(scopes));
         this.accessKeys.put(accessKey.hash(), accessKey);
         return accessKey.hash();
     }
+
     public String renewAccessKey(AccessKey accessKey) throws AccessKeyException {
-        logger.info("[Access Key Manager] Renewing access key from key: {}", accessKey);
+        logger.info("Renewing access key from key: {}", accessKey);
         return createAccessKey(accessKey.email(), accessKey.duration(), accessKey.scope());
     }
 
     public AccessKey verifyAccessKey(@NonNull String accessKey, @NonNull String endpoint) {
-        logger.info("[Access Key Manager] Verifying access key `{}`", accessKey);
+        logger.info("Verifying access key `{}`", accessKey);
 
         AccessKey key = this.accessKeys.get(accessKey);
 
         if (key == null) {
-            logger.info("[Access Key Manager] Access key not found. May be malformed or expired.");
+            logger.info("Access key not found. May be malformed or expired.");
             return null;
         }
 
-        logger.info("[Access Key Manager] Access key belongs to: {}", key.email());
-        logger.info("[Access Key Manager] Expires: {}", key.expiryTime());
-        logger.info("[Access Key Manager] Scope: {}", key.scope());
+        logger.info("Access key belongs to: {}", key.email());
+        logger.info("Expires: {}", key.expiryTime());
+        logger.info("Scope: {}", key.scope());
 
         if (key.isValid() && key.withinScope(endpoint)) return key;
         else return null;
